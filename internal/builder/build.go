f4db7bd80651509a6487c07bda9971b6614f4b57
Sergey <xxsmotur@gmail.com>
2019-09-22 16:48:25 +0500

    building of the go binary to specific platforms

--- internal/builder/build.go
+++ internal/builder/build.go
@@ -1,6 +1,7 @@
 package builder
 
 import (
+	"os"
 	"os/exec"
 	"path/filepath"
 
@@ -9,6 +10,19 @@ import (
 
 // Build provides building of the project
 func Build() error {
+	if err := buildToArch("linux", "amd64"); err != nil {
+		return err
+	}
+	if err := buildToArch("windows", "amd64"); err != nil {
+		return err
+	}
+	return nil
+}
+
+// buildToArch provides building of the go package to the specific platform
+func buildToArch(osName, platformName string) error {
+	os.Setenv("GOOS", osName)
+	os.Setenv("GOARCH", platformName)
 	err := exec.Command("go", "build", "-o", filepath.Dir(".")).Run()
 	if err != nil {
 		return errors.Wrap(err, "unable to execute go build")
