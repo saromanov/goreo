130f2a74027da8b5836796fd08caaf23b2f807c5
Sergey <xxsmotur@gmail.com>
2019-09-24 22:52:29 +0500

    git push executing

--- internal/git/git.go
+++ internal/git/git.go
@@ -4,3 +4,11 @@ type Git struct {
 	Path string
 	Release string
 }
+
+func Publish(tag string) error {
+	err := exec.Command("git", "push", "origin", tag).Run()
+	if err != nil {
+		return errors.Wrap(err, "unable to execute git push")
+	}
+	return nil
+}
