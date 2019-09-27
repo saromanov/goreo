c185d0fc47cab35f88a4ac5e4cdc7d2f5a538ce8
Sergey <xxsmotur@gmail.com>
2019-09-27 07:30:49 +0500

    oipleline generation

--- internal/pipeline/pipleline.go
+++ internal/pipeline/pipleline.go
@@ -5,3 +5,10 @@ import "github.com/saromanov/goreo/internal/config"
 type Pipleline struct {
 	conf *config.Config
 }
+
+// New initialize new pipleline
+func New(c *config.Config) *Pipleline {
+	return &Pipleline{
+		conf: c,
+	}
+}
