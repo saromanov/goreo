995724a51d2a7726098a449d4907567bf9623fbd
Sergey <xxsmotur@gmail.com>
2019-10-18 23:46:38 +0500

    unmarshaling of the config file

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/pierrre/archivefile v0.0.0-20170218184037-e2d100bc74f5
 	github.com/pkg/errors v0.8.1
 	github.com/urfave/cli v1.22.1
+	gopkg.in/yaml.v2 v2.2.2
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,30 @@
 package config
 
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
 // Config defines configuration for builders
 type Config struct {
 	build   *Build   `yaml:"build"`
 	publish *Publish `yaml:"publish"`
 	archive *Archive `yaml:"archive"`
+}
+
+// Unmarshal provides unmarshaling of the config
+func Unmarshal(path string) (*Config, error) {
+	var conf *Config
+	yamlFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	err = yaml.Unmarshal(yamlFile, &conf)
+	if err != nil {
+		return nil, err
+	}
+	return conf, nil
 }
 
 func (c *Config) GetBuild() *Build {
