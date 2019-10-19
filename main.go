d4c1cd824a9d79939ea861f4d6886241769986e3
Sergey <xxsmotur@gmail.com>
2019-10-19 20:08:00 +0500

    loading of the config

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/saromanov/goreo/internal/config"
 	"github.com/saromanov/goreo/internal/pipeline"
 	"github.com/urfave/cli"
 )
@@ -22,8 +24,12 @@ func main() {
 	}
 
 	app.Action = func(c *cli.Context) error {
+		conf, err := config.Unmarshal("config.yaml")
+		if err != nil {
+			return fmt.Errorf("unable to unmarshal config: %v", err)
+		}
 		if c.Bool(release) {
-			pipe := pipeline.New(nil)
+			pipe := pipeline.New(conf)
 			pipe.Run()
 		}
 		return nil
