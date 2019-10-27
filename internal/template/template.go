c463af1097c777ca0028e377a85c9645a678e95b
Sergey <xxsmotur@gmail.com>
2019-10-27 15:55:54 +0500

    Add Date and timestamp to template name

--- internal/template/template.go
+++ internal/template/template.go
@@ -2,24 +2,29 @@ package template
 
 import (
 	"bytes"
+	"fmt"
 	"text/template"
+	"time"
 
 	"github.com/saromanov/goreo/internal/utils"
 )
 
 type Input struct {
-	Name   string
-	Os     string
-	Tag    string
-	Date   string
-	Commit string
+	Name      string
+	Os        string
+	Tag       string
+	Date      string
+	Commit    string
+	Timestamp string
 }
 
 // GetName returns current name based on template
 func GetName(tmpl, osName string) (string, error) {
 	inp := &Input{
-		Name: utils.GetProjectName(),
-		Os:   osName,
+		Name:      utils.GetProjectName(),
+		Os:        osName,
+		Date:      time.Now().UTC().Format(time.RFC3339),
+		Timestamp: fmt.Sprintf("%d", time.Now().Unix()),
 	}
 	t, err := template.New("goreo").Parse(tmpl)
 	if err != nil {
