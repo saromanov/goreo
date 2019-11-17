1fde3b97e369dfdde998396b340bd216fdeb3fbb
Sergey <xxsmotur@gmail.com>
2019-11-17 17:21:37 +0500

    Add methods to artifacts

--- internal/artifact/artifact.go
+++ internal/artifact/artifact.go
@@ -1,5 +1,7 @@
 package artifact
 
+import "sync"
+
 type Type int
 
 const (
@@ -8,7 +10,35 @@ const (
 	DockerImage
 )
 
+func (t Type) String() string {
+	switch t {
+	case DockerImage:
+		return "Docker Image"
+	case UploadableArchive:
+		return "Archive"
+	case UploadableBinary:
+		return "Binary"
+	}
+	return "unknown"
+}
+
 type Artifact struct {
 	Name    string
 	Version string
+}
+
+type Artifacts struct {
+	items []*Artifact
+	lock  *sync.Mutex
+}
+
+func New() Artifacts {
+	return Artifacts{
+		items: []*Artifact{},
+		lock:  &sync.Mutex{},
+	}
+}
+
+func (artifacts Artifacts) List() []*Artifact {
+	return artifacts.items
 }
