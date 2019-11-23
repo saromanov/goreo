36c4f7713281259675a35346451d8ea5f7a2ce5b
Sergey <xxsmotur@gmail.com>
2019-11-23 21:20:15 +0500

    Add interface for publisher

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/saromanov/goreo
 go 1.13
 
 require (
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/pierrre/archivefile v0.0.0-20170218184037-e2d100bc74f5
 	github.com/pkg/errors v0.8.1
 	github.com/sirupsen/logrus v1.4.2
--- internal/publisher/publisher.go
+++ internal/publisher/publisher.go
@@ -1,5 +1,15 @@
 package publisher
 
-type Publisher interface{
-	
+import (
+	"context"
+	"github.com/google/go-github/github"
+)
+
+type RepositoryRelease struct {
+
+}
+// Publisher defines uploading to services
+type Publisher interface {
+	CreateRelease(ctx context.Context, req RepositoryRelease) (RepositoryRelease, error)
+	GetRelease(ctx context.Context, tag string) (RepositoryRelease, error)
 }
